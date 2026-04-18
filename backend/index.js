90430c350f13e096e16fad4cdabf7480b7d8f02c
sahil <itamiedits380@gmail.com>
2026-04-18 10:30:29 +0530

    fixed cors

--- backend/index.js
+++ backend/index.js
@@ -34,8 +34,8 @@ const User = mongoose.model("User", userSchema);
 // ================= MIDDLEWARE =================
 app.use(cors({
   origin:
-  [ process.env.FRONTEND_URL ,
-     "http://localhost:5173"],
+  [ "https://bookwebsite-eta.vercel.app" ,
+     "http://localhost:5173" ],
   credentials: true
 }));
 
